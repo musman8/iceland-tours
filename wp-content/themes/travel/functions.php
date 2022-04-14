6da5407fbf5e30e0a3ce85a29ab163f95abbd7ae
Muhammad Usman <100186492+musman8@users.noreply.github.com>
2022-04-14 16:32:03 +0500

    Merge - improve: navigation added, not looking to make it responsive at the moment (#2)

--- wp-content/themes/travel/functions.php
+++ wp-content/themes/travel/functions.php
@@ -3,6 +3,7 @@
     // Load in CSS
     function travel_enqueue_styles() {
         wp_enqueue_style( 'home', get_stylesheet_directory_uri() . '/assets/css/home.css', [], time(), 'all' );
+        wp_enqueue_style( 'navigation', get_stylesheet_directory_uri() . '/assets/css/navigation.css', [], time(), 'all' );
     }
     add_action( 'wp_enqueue_scripts', 'travel_enqueue_styles' );
 ?>
