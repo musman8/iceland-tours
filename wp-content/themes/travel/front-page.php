d90b1b5332e6b5b29e1abe3f1ad247b5349a74ad
Muhammad Usman <muhammad.usman8@keeptruckin.com>
2022-04-14 18:14:08 +0500

    improve: image, heading added

--- wp-content/themes/travel/front-page.php
+++ wp-content/themes/travel/front-page.php
@@ -1,7 +1,19 @@
 <?php get_header(); ?>
 
-<main>
-    <p>This is the front page!</p>
+<main class="main-container">
+    <div class="hero-image-container">
+        <img class="hero-image" src="<?php echo get_template_directory_uri() . '/assets/images/hero.jpg' ?>" alt="hero" />
+    </div>
+
+    <div class="image-mist-overlay"></div>
+
+    <div class="main-headings">
+        <h1 class="primary">ice</h1>
+        <h1 class="secondary">land</h1>
+    </div>
+
+    
+    
 </main>
 
 <?php get_footer(); ?>
