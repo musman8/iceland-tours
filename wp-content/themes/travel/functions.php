53020a40037094e5c497d22a1b7d7a163668bb3d
Muhammad Usman <100186492+musman8@users.noreply.github.com>
2022-04-14 18:15:39 +0500

    Merge - improve: image, heading added (#3)

--- wp-content/themes/travel/functions.php
+++ wp-content/themes/travel/functions.php
@@ -4,6 +4,7 @@
     function travel_enqueue_styles() {
         wp_enqueue_style( 'home', get_stylesheet_directory_uri() . '/assets/css/home.css', [], time(), 'all' );
         wp_enqueue_style( 'navigation', get_stylesheet_directory_uri() . '/assets/css/navigation.css', [], time(), 'all' );
+        wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all' );
     }
     add_action( 'wp_enqueue_scripts', 'travel_enqueue_styles' );
 ?>
