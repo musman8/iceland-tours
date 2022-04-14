138e4ea7634f9c7edab64534fd28f6be29bb143f
Muhammad Usman <muhammad.usman8@keeptruckin.com>
2022-04-14 16:07:28 +0500

    improve: initial files have been set up

---
+++ wp-content/themes/travel/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iceland Tours</title>
+</head>
+<body>
+    <?php wp_head(); ?>
+    <div class="navigation-container"> <!-- #navigation start -->
+        <div> <!-- #navigation social links -->
+            <div>
+                <p>Follow Us</p>
+                <p><span class="dashicons dashicons-whatsapp"></span></p>
+                <p><span class="dashicons dashicons-facebook-alt"></span></p>
+                <p><span class="dashicons dashicons-youtube"></span></p>
+                <p><span class="dashicons dashicons-email"></span></p>
+            </div>
+            <div>
+
+            </div>
+        </div> <!-- #navigation social links -->
+        <div> <!-- #navigation pages -->
+
+            <p>Destinations</p>
+            <p>Deals</p>
+            <p>Special offers</p>
+            <p>About us</p>
+
+        </div> <!-- #navigation pages -->
+    </div> <!-- #navigation end -->
